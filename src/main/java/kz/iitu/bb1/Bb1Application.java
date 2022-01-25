5f72f6fc8adef1c09dd557ee33b223fd0bd986db
unknown <rautov74@gmail.com>
2022-01-25 15:35:35 +0600

    pingtask

--- src/main/java/kz/iitu/bb1/Bb1Application.java
+++ src/main/java/kz/iitu/bb1/Bb1Application.java
@@ -2,8 +2,10 @@ package kz.iitu.bb1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Bb1Application {
 
     public static void main(String[] args) {
